ae9cc3ae6d934030da158e26fafcb47863a78ee6
Anett Bianka Barát <58390834+Kilgarra@users.noreply.github.com>
2022-05-09 14:41:29 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Task} from "./task/task";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sep6-app';
+  constructor() {
+  }
+  Search(){
+  }
+  todo: Task[] = [
+    {
+      title: 'Buy milk',
+      description: 'Go to the store and buy milk'
+    },
+    {
+      title: 'Create a Kanban app',
+      description: 'Using Firebase and Angular create a Kanban app!'
+    },
+    {
+      title: 'Whatever',
+      description: 'Does this work?'
+    }
+  ];
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'sep6-2dccd',
+    appId: '1:395567603857:web:fc83a6648878bb7bfcdf30',
+    storageBucket: 'sep6-2dccd.appspot.com',
+    apiKey: 'AIzaSyDAyuJENUhz9hR0YGG085M54Bds4h0glro',
+    authDomain: 'sep6-2dccd.firebaseapp.com',
+    messagingSenderId: '395567603857',
+    measurementId: 'G-KZF0HM9YNJ',
+  },
   production: true
 };

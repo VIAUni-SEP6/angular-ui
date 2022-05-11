5a631c75e235b96c3cce1c9a9b632a0e5bccc7c4
Anett Bianka Barát <58390834+Kilgarra@users.noreply.github.com>
2022-05-11 17:01:45 +0200

    displaying movie titles after search

--- src/app/shared/services/tmdb.service.ts
+++ src/app/shared/services/tmdb.service.ts
@@ -29,12 +29,13 @@ export interface MovieApiObject {
   providedIn: 'root'
 })
 export class TmdbService {
-  public baseUrl = 'https://api.themoviedb.org/3/';
+  public baseUrl = 'https://api.themoviedb.org/3';
   private apiKey = '1629f55fe2e85b91bf3b83337f2a7291';
 
   constructor(private httpClient: HttpClient) { }
   public searchMovies(searchText: string): Observable<any> {
-    return this.httpClient.get<SearchResultApiObject>(this.baseUrl+'/search/movie?api_key='+this.apiKey+'&query='+searchText);
+    return this.httpClient.get<SearchResultApiObject>(
+      this.baseUrl+'/search/movie?api_key='+this.apiKey+'&query='+searchText.valueOf());
   }
 
   // private apiObjectToSearchResult(result: SearchResultApiObject) {

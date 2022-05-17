22cc3ef67b78507f6ff6ca41557e3d32c46bd615
Anett Bianka Barát <58390834+Kilgarra@users.noreply.github.com>
2022-05-17 18:24:46 +0200

    movie details almost finished

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,19 +1,23 @@
 import {OnInit, Component} from '@angular/core';
-import {SearchResultApiObject, TmdbService} from "../../shared/services/tmdb.service";
+import {MovieApiObject, SearchResultApiObject, TmdbService} from "../../shared/services/tmdb.service";
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {MovieDetailComponent} from "../movie-detail/movie-detail.component";
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit{
+export class SearchComponent implements OnInit {
   searchFormGroup: FormGroup;
   searchTextControlName = 'searchText';
   hasResults = false;
   movies: SearchResultApiObject;
+  private dialogRef: MatDialogRef<MovieDetailComponent, MovieApiObject>;
 
-  constructor(public tmdbService: TmdbService,public formBuilder: FormBuilder) { }
+  constructor(public tmdbService: TmdbService, public formBuilder: FormBuilder, private dialog: MatDialog) {
+  }
 
   ngOnInit(): void {
     this.buildFormGroup();
@@ -21,10 +25,20 @@ export class SearchComponent implements OnInit{
 
   searchMovies() {
     this.tmdbService.searchMovies(
-      this.searchFormGroup.get(this.searchTextControlName)?.value).subscribe((value) => {
-        this.hasResults=true;
-        this.movies = value;
-      })
+      this.searchFormGroup.get(this.searchTextControlName)?.value).subscribe((searchResult: SearchResultApiObject) => {
+      this.hasResults = true;
+      this.movies = searchResult;
+    })
+  }
+
+  openMovieDetails(movie: MovieApiObject) {
+    this.dialogRef = this.dialog.open(MovieDetailComponent, {
+      autoFocus: false,
+      data: movie,
+      width: '900px',
+      height: '700px'
+    });
+
   }
 
   private buildFormGroup(): void {
--- src/app/shared/services/backend.service.ts
+++ src/app/shared/services/backend.service.ts
@@ -15,25 +15,25 @@ export interface ToplistApiObject {
 })
 export class BackendService {
   public baseUrl = 'https://us-central1-sep6-database.cloudfunctions.net/expressApi/';
-  private apiKey = '1629f55fe2e85b91bf3b83337f2a7291';
+  //private apiKey = '1629f55fe2e85b91bf3b83337f2a7291';
 
   constructor(private httpClient: HttpClient, public authService:AuthService) { }
 
-  public getToplist(): Observable<any>{
+  public getToplist(): Observable<ToplistResultApiObject>{
     let url = this.baseUrl + 'getFavouriteMovies';
     let token = this.authService.userIdToken;
     return this.httpClient.get<ToplistResultApiObject>(url,{ headers: {"Authorization" : `Bearer ${token}`}});
   }
 
-  public addToplist(toplistID: string): Observable<any> {
-      let url = this.baseUrl+'addFavouriteMovie'+'/'+toplistID.valueOf();
+  public addMovieToToplist(movieId: string): Observable<any> {
+      let url = this.baseUrl+'addFavouriteMovie'+'/'+movieId.valueOf();
       let token = this.authService.userIdToken;
       const headers = {"Authorization" : `Bearer ${token}`};
       return this.httpClient.get<any>(url, { headers});
     }
 
-    public deleteToplist(toplistID2: string): Observable<any> {
-          let url = this.baseUrl+'deleteFavouriteMovie'+'/'+toplistID2.valueOf();
+    public deleteMovieFromToplist(movieId: string): Observable<any> {
+          let url = this.baseUrl+'deleteFavouriteMovie'+'/'+movieId.valueOf();
           let token = this.authService.userIdToken;
           const headers = {"Authorization" : `Bearer ${token}`};
           return this.httpClient.get<any>(url, { headers});
